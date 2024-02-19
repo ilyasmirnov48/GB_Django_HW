7b5c7f6dbe0aa8dbf1858c9901ba425b8a941fc3
ilya48 <ilyasmirnov48@mail.ru>
2024-02-19 16:42:01 +0300

    HW_4 completed

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+
+from . import models, forms
 from .models import Client, Order
 from django.http import HttpRequest
 from datetime import date, timedelta
+from django.core.files.storage import FileSystemStorage
+from myapp.forms import ImageForm
 
 
 def index(request):
@@ -31,3 +35,37 @@ def product_time(request: HttpRequest, pk):
     orders_year = Order.objects.filter(customer=user, date_ordered__gte=year_delta).all()
     return render(request, 'myapp/product_time.html',
                   {'orders_week': orders_week, 'orders_month': orders_month, 'orders_year': orders_year})
+
+
+def change_product(request, product_id):
+    product = models.Product.objects.filter(pk=product_id).first()
+    form = forms.ProductForm(request.POST, request.FILES)
+    if request.method == 'POST' and form.is_valid():
+        product.name = form.cleaned_data['name']
+        product.description = form.cleaned_data['description']
+        product.price = form.cleaned_data['price']
+        product.quantity = form.cleaned_data['quantity']
+        if product.image is None:
+            image = form.cleaned_data['image']
+            fs = FileSystemStorage()
+            fs.save(image.name, image)
+        else:
+            product.image = form.cleaned_data['image']
+        product.save()
+    else:
+        form = forms.ProductForm(initial={'name': product.name, 'description': product.description,
+                                          'price': product.price, 'quantity': product.quantity, 'image': product.image})
+
+    return render(request, 'myapp/change_product.html', {'form': form})
+
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data['image']
+            fs = FileSystemStorage()
+            fs.save(image.name, image)
+    else:
+        form = ImageForm()
+    return render(request, 'myapp/upload_image.html', {'form': form})
